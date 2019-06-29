5a4e2f5e49a605607bb83636eb24c8a39227ec75
rush <rushastronomo0415@gmail.com>
2019-06-28 17:12:49 -0700

    new .circleci, modified ExpenseEntries, IncomeEntries, and Summary

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,9 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <div className='jumbotron' >
           <h1 className='display-3 text-center'>Budget Tracker</h1>
-        </div>
         <div className='row'>
           <div className='col-12 col-md-6 mb-4'>
             <IncomeEntries />
--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,39 +1,78 @@
 import React from 'react';
 
+// We'll need to import all those action creators.
+import {
+  updateIncomeDescription,
+  updateIncomeAmount,
+  addIncome
+} from './incomeActions';
 
 export default class IncomeEntries extends React.Component {
   constructor(props) {
     super(props);
+
+    // Here we're binding these methods to the context
+    // of the components. This only has to be done,
+    // because these methods are called back by
+    // event emitters (which lose context).
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddExpense = this.handleAddExpense.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    // dispatch was provided by connect()
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeAmount(value));
+  }
+
+  handleAddExpense() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addIncome(description, amount));
   }
 
   render() {
+    // These values were provided by connect()
+    const { description, amount, lineItems } = this.props;
     return (
       <div className='card border-success mb-3'>
         <div className='card-header text-white bg-success'>Income Entries</div>
         <div className='card-body'>
           <form>
             <div className='form-group'>
-              <label htmlFor='Income-description'>Description</label>
+              <label htmlFor='income-description'>Description</label>
               <input
                 type='text'
                 className='form-control'
-                id='Income-description'
+                id='income-description'
+                value={ description }
+                onChange={ this.handleDescriptionInput }
               />
             </div>
             <div className='form-group'>
-              <label htmlFor='Income-amount'>Amount</label>
+              <label htmlFor='income-amount'>Amount</label>
               <div className='input-group'>
                 <span className='input-group-addon'>$</span>
                 <input
                   type='text'
                   className='form-control'
-                  id='Income-amount'
+                  id='income-amount'
+                  value={ amount }
+                  onChange={ this.handleAmountInput }
                 />
               </div>
             </div>
             <button
               type='button'
               className='btn btn-success col-12 mb-5'
+              onClick={ this.handleAddExpense }
             >+ Add Income
             </button>
             <table className='table table-sm table-hover'>
@@ -44,10 +83,14 @@ export default class IncomeEntries extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Rent</td>
-                  <td>$1,500.00</td>
-                </tr>
+                {
+                  lineItems.map(lineItem => (
+                    <tr>
+                      <td>{ lineItem.description }</td>
+                      <td>${ lineItem.amount.toFixed(2) }</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </form>
@@ -56,4 +99,3 @@ export default class IncomeEntries extends React.Component {
     );
   }
 }
-
